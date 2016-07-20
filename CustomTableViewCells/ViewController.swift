a6eb4984bdcb96fd20193c1b5ae264653a5e4df9
Joseph DeCarlo <josephdecarlo@bziti.com>
2016-07-20 19:07:20 -0400

    I added TableView Settings
    Detailed Description

--- CustomTableViewCells/ViewController.swift
+++ CustomTableViewCells/ViewController.swift
@@ -8,11 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    let data = ["Red", "Blue", "Green", "Orange", "Brown"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.tableView.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +24,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+     
+        return data.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        if let cell = tableView.dequeueReusableCellWithIdentifier("MyReuseableCellID", forIndexPath: indexPath) as? MyCustomTableViewCell {
+
+            cell.myLabel.text = data[indexPath.row]
+            
+            return cell
+        }
+        
+        return UITableViewCell()
+    }
 
 }
 
